c350c819b66e8aa6c870487fee97c672162abfca
Rachel Bonanno <72635627+rachelBonanno@users.noreply.github.com>
2022-09-26 09:39:26 -0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,16 +1,19 @@
 # Python program to find the factorial of a number provided by the user.
-
 # To take input from the user
-num = int(input("Enter a number: "))
+# int(input("Enter a number: "))
 
+num = [10, 20, 30, 40, 50]
 factorial = 1
 
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+for val in num:
+    # check if the number is negative, positive or zero
+    if val < 0:
+       print("Sorry, factorial does not exist for negative numbers")
+    elif val == 0:
+       print("The factorial of 0 is 1")
+    else:
+       for i in range(1,val + 1):
+           factorial = factorial*i
+       print("The factorial of",val,"is",factorial)
+    factorial = 1
+
